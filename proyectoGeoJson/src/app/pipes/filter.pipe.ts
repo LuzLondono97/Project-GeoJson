f54747738f3566f278371d4aa1dd2e615f788d6e
Luz Londono <luza.londono@premize.com>
2020-08-11 11:46:38 -0500

    Se ajusta el filtro del número del documento del  usuario

--- proyectoGeoJson/src/app/pipes/filter.pipe.ts
+++ proyectoGeoJson/src/app/pipes/filter.pipe.ts
@@ -6,9 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
 
   transform(value: any, args: any): any {
+    if(args == '' || args.length < 3) return value;
     const resultUsers = [];
     for(const user of value){
-      if(user.numero_identificacion_id.indexOf(args) > -1){
+      if(user.numero_identificacion_id.toLowerCase().indexOf(args.toLowerCase()) > -1){
         resultUsers.push(user);
       };
     };
